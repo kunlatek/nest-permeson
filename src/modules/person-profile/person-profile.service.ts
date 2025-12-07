3a3122233dab8b1c3054ded891e0d1b5f1d3a24f
Alexis de Barros <apenasalexis@gmail.com>
2025-12-07 00:21:11 -0300

    :bug: handle null profile cases in findProfileById methods for CompanyProfileService and PersonProfileService

--- src/modules/person-profile/person-profile.service.ts
+++ src/modules/person-profile/person-profile.service.ts
@@ -58,6 +58,7 @@ export class PersonProfileService {
 
     async findProfileById(id: string, lang: string): Promise<{_id: string, userId: string, userName: string}> {
         const profile = await this.personProfileRepository.findById(id)
+        if (!profile) return null
         return {_id: profile._id, userId: profile.userId, userName: profile.userName}
     }
 }
